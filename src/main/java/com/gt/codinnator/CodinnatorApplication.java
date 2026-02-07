27d95d255111ad68f1c7b7e802369e944bae23ca
121593029 <user@codinnator.com>
2026-02-07 12:32:04 +0000

    test

--- src/main/java/com/gt/codinnator/CodinnatorApplication.java
+++ src/main/java/com/gt/codinnator/CodinnatorApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodinnatorApplication {
 
 	public static void main(String[] args) {
-           SpringApplication.run(CondinnatorApplication.class,args);
+           SpringApplication.run(CondinatorApplication.class,args);
 }
 
 }
